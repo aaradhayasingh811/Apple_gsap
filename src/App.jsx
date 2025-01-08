12a9c08cc13dde0953e1af153098cdff3619e08c
user.name : aaradhayasingh811 <user.email : aaradhayasingh811@gmail.com>
2025-01-09 00:30:39 +0530

    added model

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import Video from './components/Video'
 import Explore from './components/Explore'
 import VideoCara from './components/VideoCara'
+import Model from './components/Model'
 function App() {
  
   return (
@@ -16,6 +17,7 @@ function App() {
       <Highlights/>
       {/* <VideoCara/> */}
       <Explore/>
+      <Model/>
       <Video/>
       <Footer/>
     </div>
---
+++ src/components/Model.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import a from '../assets/images/black.jpg';
+import b from '../assets/images/blue.jpg';
+import c from '../assets/images/white.jpg';
+import d from '../assets/images/yellow.jpg';
+import frame from '../assets/images/frame.png';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Model = () => {
+  useGSAP(() => {
+    gsap.to('.play', {
+      duration: 2,
+      y: -50,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.play',
+        start: 'top 80%',
+        end: 'bottom 20%',
+        scrub: 2,
+      },
+    });
+  }, []);
+
+  // Update image src dynamically
+  const handleColorChange = (imageSrc) => {
+    const imgElement = document.getElementById('mainImage');
+    if (imgElement) {
+      imgElement.src = imageSrc;
+    }
+  };
+
+  return (
+    <div className="bg-[#101010] overflow-x-hidden w-full relative">
+      <div className="sm:px-44 px-4 py-5">
+        <h1 className="text-5xl text-[#86868b] py-20 play font-semibold opacity-0 translate-y-15">
+          Take a Closer Look.
+        </h1>
+      </div>
+      <div className="flex flex-col justify-center items-center mx-auto rounded-lg relative my-20 h-[60vh] sm:h-[80vh]">
+        {/* Frame */}
+        <img
+          src={frame}
+          alt="Frame"
+          className="bg-transparent relative sm:h-[40vh] sm:w-[70vw] h-[33vh] w-[72vw] lg:h-[44vh] z-10 rotate-90 lg:w-[37vw] md:h[44vh] md:w[30vw] overflow-hidden"
+        />
+        {/* Image inside the frame */}
+        <img
+          id="mainImage"
+          src={a}
+          alt="Main"
+          className="absolute sm:h-[40vh] sm:w-[70vw] h-[30vh] w-[70vw] lg:w-[35vw] lg:h-[40vh] object-cover sm:rounded-[30px] rounded-[14px] rotate-90 md:h[44vh] md:w[30vw]"
+        />
+      </div>
+
+      {/* Color Palette */}
+      <div className="palette my-20 flex justify-center ">
+        <div className=" bg-[#424245] w-max flex rounded-full p-2">
+          {/* Buttons for changing colors */}
+          <button
+            style={{ borderRadius: '50%' }}
+            className="mx-4 w-7 h-7 sm:w-10 sm:h-10 bg-[#1B1B1B]"
+            onClick={() => handleColorChange(a)}
+          ></button>
+          <button
+            style={{ borderRadius: '50%' }}
+            className="mx-4 w-7 h-7 sm:w-10 sm:h-10 bg-[#2F4452]"
+            onClick={() => handleColorChange(b)}
+          ></button>
+          <button
+            style={{ borderRadius: '50%' }}
+            className="mx-4 w-7 h-7 sm:w-10 sm:h-10 bg-[#DDDDDD]"
+            onClick={() => handleColorChange(c)}
+          ></button>
+          <button
+            style={{ borderRadius: '50%' }}
+            className="mx-4 w-7 h-7 sm:w-10 sm:h-10 bg-[#E5E0C1]"
+            onClick={() => handleColorChange(d)}
+          ></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Model;
